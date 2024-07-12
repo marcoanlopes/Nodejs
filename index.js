5eb3817bc81f320020b46c7e848480392ecd7ed3
Marco Junior <marcoalopes@outlook.com>
2024-07-11 23:40:15 -0300

    "fix console log"

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import * as cowsay from "cowsay";
 
 let output = cowsay.say({"text":'Mooooooo choooooo!'});
 //let output2 = cowsay.think({"text": "mooooooo "})
-console.log(output1);
+console.log(output);
